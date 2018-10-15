cea5ab0bf81ca816379ce0e3d5e30745e5ec7821
gyarasu <dev.yoshitsugu@gmail.com>
2018-10-15 17:34:21 +0900

    feat: add mysql as session store

--- server/index.js
+++ server/index.js
@@ -5,17 +5,33 @@ process.env.NODE_CONFIG_DIR = `${__dirname}/config`;
 const { Nuxt, Builder } = require('nuxt');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const app = require('express')();
 const config = require('config');
 
 app.use(bodyParser.json());
 
 // create `req.session`
+const sessionStore = new MySQLStore(Object.assign(
+  {},
+  config.get('mysql'),
+  {
+    clearExpired: true,
+    checkExpirationInterval: 900000,
+    expiration: 86400000,
+    createDatabaseTable: true,
+    endConnectionOnClose: true,
+  }
+));
+
 app.use(session({
-  secret: 'super-secret-key',
+  secret: 'your-secret',
   resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 6000 },
+  store: sessionStore,
+  saveUninitialized: true,  // TODO: Edit this depends on use case.
+  cookie: {
+    maxAge: 86400000,
+  },
 }));
 
 const isProd = !process.env.NODE_ENV === 'development';
