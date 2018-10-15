974a2ebc2c5df6c6bfe4b943962e740456e87bc4
gyarasu <dev.yoshitsugu@gmail.com>
2018-10-15 18:35:50 +0900

    feat: add sample db access, store login history

---
+++ server/models/index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const config = require('config');
+
+const conf = {
+  host: config.get('mysql.host'),
+  user: config.get('mysql.user'),
+  password: config.get('mysql.password'),
+  database: config.get('mysql.database'),
+};
+
+const connection = mysql.createConnection(conf);
+
+module.exports = {
+  conf,
+  connection,
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const config = require('config');
+const { connection } = require('../models');
 
 const router = Router();
 
@@ -11,13 +12,32 @@ const dummyPassWord = '123123';
 router.post('/api/auth/login', (req, res, next) => {
   if (req.body.email === dummyEmail && req.body.password === dummyPassWord) {
     req.session.authUser = { username: req.body.email };
-    return res.status(200).json({
-      status: 'ok',
+
+    // insert latest login
+    const query = connection.query('INSERT INTO t_latest_login_history SET ? ON DUPLICATE KEY UPDATE ?', [{
+      user_id: req.body.email,
+      create_date: new Date(),
+      update_date: new Date(),
+    }, {
+      update_date: new Date(),
+    }], (error) => {
+      if (error) {
+        console.log('error', error);
+        // TODO: Replace it for Error Handler
+        return res.status(500).json({
+          status: 'Something Wrong on Server.',
+        });
+      } else {
+        return res.status(200).json({
+          status: 'ok',
+        })
+      }
+    });
+  } else {
+    return res.status(401).json({
+      error: 'Bad credentials',
     });
   }
-  return res.status(401).json({
-    error: 'Bad credentials',
-  });
 });
 
 
