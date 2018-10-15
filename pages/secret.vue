9b8f3205e25209fdcf164cc4fc7a73112e67ba71
gyarasu <dev.yoshitsugu@gmail.com>
2018-10-15 18:15:20 +0900

    feat: replace login check process for middleware

--- pages/secret.vue
+++ pages/secret.vue
@@ -7,10 +7,6 @@
 
 <script>
 export default {
-  fetch ({ store, redirect }) {
-    if (!store.state.authUser) {
-      return redirect('/');
-    }
-  },
+  middleware: 'authenticated',
 };
 </script>
