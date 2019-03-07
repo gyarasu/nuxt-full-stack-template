9c0635ca9d12e7f8f556aa75cc6d0f7aaadbe41b
gyarasu <dev.yoshitsugu@gmail.com>
2019-03-07 16:00:37 +0900

    feat: wrap routes in try-catch

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,45 +10,53 @@ const dummyPassWord = '123123';
 
 // LOGIN API
 router.post('/api/auth/login', (req, res, next) => {
-  if (req.body.email === dummyEmail && req.body.password === dummyPassWord) {
-    req.session.authUser = { email: req.body.email };
+  try {
+    if (req.body.email === dummyEmail && req.body.password === dummyPassWord) {
+      req.session.authUser = { email: req.body.email };
 
-    // insert latest login
-    const query = connection.query('INSERT INTO t_latest_login_history SET ? ON DUPLICATE KEY UPDATE ?', [{
-      user_id: req.body.email,
-      create_date: new Date(),
-      update_date: new Date(),
-    }, {
-      update_date: new Date(),
-    }], (error) => {
-      if (error) {
-        next(error);
-      } else {
-        return res.status(200).json({
-          status: 'ok',
-        })
-      }
-    });
-  } else {
-    return res.status(401).json({
-      error: 'Bad credentials',
-    });
+      // insert latest login
+      const query = connection.query('INSERT INTO t_latest_login_history SET ? ON DUPLICATE KEY UPDATE ?', [{
+        user_id: req.body.email,
+        create_date: new Date(),
+        update_date: new Date(),
+      }, {
+        update_date: new Date(),
+      }], (error) => {
+        if (error) {
+          next(error);
+        } else {
+          return res.status(200).json({
+            status: 'ok',
+          })
+        }
+      });
+    } else {
+      return res.status(401).json({
+        error: 'Bad credentials',
+      });
+    }
+  } catch(err) {
+    next(err);
   }
 });
 
 
 // LOGOUT API
 router.post('/api/auth/logout', (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) {
-      // TODO: Replace Error Log
-      console.log('error', err);
-    }
-  });
+  try {
+    req.session.destroy((err) => {
+      if (err) {
+        // TODO: Replace Error Log
+        console.log('error', err);
+      }
+    });
 
-  return res.json({
-    status: 'ok',
-  });
+    return res.json({
+      status: 'ok',
+    });
+  } catch(err) {
+    next(err);
+  }
 });
 
 module.exports = router;
